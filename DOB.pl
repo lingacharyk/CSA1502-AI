205c59ef00bf2108a0d190ac6389903df0daa133
lingacharyk <113505257+lingacharyk@users.noreply.github.com>
2023-10-19 08:18:36 +0530

    Add files via upload

---
+++ DOB.pl
@@ -0,0 +1,8 @@
+born(leo, 20,3,1977).
+born(kesari, 2,2,1992).
+born(tiger, 17,3,1995).
+born(mad, 1,1,2004).
+born(nageshwar, 24,12,2000).
+born(rao, 30,4,1989).
+born(baghavath, 17,3,1993).
+born(nelakonda, 16,11,1995).
---
+++ students.pl
@@ -0,0 +1,3 @@
+class(ram,rama,csa12).
+class(ramu,ramesh,csa14).
+class(shankar,veera,eca04).
